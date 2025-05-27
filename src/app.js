d3f76a298db4bdcf214f8bfa167d1429892a5e64
Harshal Bhosale <134089001+codehb01@users.noreply.github.com>
2025-05-28 03:13:50 +0530

    Healthchecks routes and testing

--- src/app.js
+++ src/app.js
@@ -13,6 +13,12 @@ app.use(
 );
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
-app.use(express.static("public"))
+app.use(express.static("public"));
+
+// import routes
+import healthcheckRouter from "./routes/healthcheck.routes.js";
+
+// create routes
+app.use("/api/v1/healthcheck",healthcheckRouter);
 
 export { app };
