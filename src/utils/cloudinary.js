b0b2727a34e76abbdbc964958ebe10ba42990461
Harshal Bhosale <134089001+codehb01@users.noreply.github.com>
2025-05-30 02:23:32 +0530

    Multer and Cloudinary file handling

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,30 @@
+// Handling file with cloudinary
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+// Configuration of cloudinary
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET, // Click 'View API Keys' above to copy your API secret
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) return null; //checking here
+    //upload code
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+    console.log("File uploaded on cloudinary. File src: " + response.url);
+    // once file is uploaded,we would delete it from the server
+    fs.unlinkSync(localFilePath);
+    return response;
+
+  } catch (error) {
+    fs.unlinkSync(localFilePath);
+    return null; //used to remove a file or symbolic link from the filesystem
+  }
+};
+
+export { uploadOnCloudinary };
